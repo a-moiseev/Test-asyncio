e810760b4df68341b09a41153c542911bebc4b75
Alexey Moiseev <alexey@moiseev.pro>
2021-03-28 19:27:02 +0300

    Добавлена обработка сигналов SIGINT, SIGTERM

--- test-asyncio.py
+++ test-asyncio.py
@@ -1,12 +1,14 @@
 import asyncio
 import random
+import signal
+import functools
 
 # Константы, минимальное и максимальное число
 NUMBER_MIN = 1
 NUMBER_MAX = 1_000_000
 
 
-async def coroutine_1(queue_2, queue_3, queue_4):
+async def coroutine_1(queues):
     """
     Корутина 1 cо случайной периодичностью от 1 до 10 сек.
     генерирует случайное число от NUMBER_MIN до NUMBER_MAX.
@@ -16,14 +18,13 @@ async def coroutine_1(queue_2, queue_3, queue_4):
         await asyncio.sleep(random.randint(1, 10))
         number = random.randint(NUMBER_MIN, NUMBER_MAX)
         # print(f'Coroutine_1 {number} Put')
-        queue_2.put_nowait(number)
-        queue_3.put_nowait(number)
-        queue_4.put_nowait(number)
+        for queue in queues:
+            queue.put_nowait(number)
 
 
 async def coroutine_2(queue):
     """
-    Получает из очереди-2 число.
+    Получает из очереди число.
     Вычисляет максимальное число, если максимальное число изменилось,
     выводит его на консоль.
     """
@@ -37,7 +38,7 @@ async def coroutine_2(queue):
 
 async def coroutine_3(queue):
     """
-    Получает из очереди-3 число.
+    Получает из очереди число.
     Вычисляет минимальное число, если минимальное число изменилось,
     выводит его на консоль.
     """
@@ -51,7 +52,7 @@ async def coroutine_3(queue):
 
 async def coroutine_4(queue):
     """
-    Получает из очереди-4 число.
+    Получает из очереди число.
     Вычисляет среднее число, если среднее число изменилось,
     выводит его на консоль.
     """
@@ -67,16 +68,31 @@ async def coroutine_4(queue):
             print(f'Average = {average_number}')
 
 
+def ask_exit(signame, loop):
+    """
+    Останавливает корутины
+    """
+    print(f'Got signal {signame}: exit')
+    loop.stop()
+
+
 # Создаются три очереди
-queue_2 = asyncio.Queue()
-queue_3 = asyncio.Queue()
-queue_4 = asyncio.Queue()
+queues = []
+for i in range(3):
+    queue = asyncio.Queue()
+    queues.append(queue)
 
 loop = asyncio.get_event_loop()
-tasks = [
-    loop.create_task(coroutine_1(queue_2, queue_3, queue_4)),
-    loop.create_task(coroutine_2(queue_2)),
-    loop.create_task(coroutine_3(queue_3)),
-    loop.create_task(coroutine_4(queue_4))
-]
+loop.create_task(coroutine_1(queues))
+loop.create_task(coroutine_2(queues[0]))
+loop.create_task(coroutine_3(queues[1]))
+loop.create_task(coroutine_4(queues[2]))
+
+# Обработчик сигналов SIGINT, SIGTERM
+for signame in {'SIGINT', 'SIGTERM'}:
+    loop.add_signal_handler(
+        getattr(signal, signame),
+        functools.partial(ask_exit, signame, loop)
+    )
+
 loop.run_forever()
